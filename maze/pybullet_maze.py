0e703bc86fe377fc4464bd63fefe84d2de885f6a
scott bennett <sbphone302@gmail.com>
2023-02-25 07:38:35 -0800

    Update and rename pybullet_maze.py to maze/pybullet_maze.py

--- maze/pybullet_maze.py
+++ maze/pybullet_maze.py
@@ -7,9 +7,9 @@ p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 startPos = [0,0,1]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-mazeId = p.loadURDF("/home/reed/ros2_ws_1.1/src/urdf_fnl_robot/robot.urdf", basePosition=[0,0,0])
+mazeId = p.loadURDF("PUT YOUR FILE LOCATION HERE.urdf", basePosition=[0,0,0])
 #boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
-carId = p.loadURDF("/home/reed/ros2_ws_1.1/src/urdf_car_body/robot.urdf", basePosition=[0,0,0.2])
+#carId = p.loadURDF("/home/reed/ros2_ws_1.1/src/urdf_car_body/robot.urdf", basePosition=[0,0,0.2])
 #set the center of mass frame (loadURDF sets base link frame) s>
 while True:
 #       for i in range (10000):
